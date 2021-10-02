a9e30e96681026c6c5303f51f3e941358dbebe5e
Renan Luz <ren4n@live.com>
2021-10-02 12:24:58 -0300

    insert implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,10 +1,20 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.DepartmentDao;
+import model.entities.Department;
+
 public class Program2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
+		
+		
+		System.out.println("\n===== TEST 1: department insert =====");	
+		Department newDepartment = new Department(null, "Kitchen");
+		departmentDao.insert(newDepartment); ;
+		System.out.println("Inserted! New id = " + newDepartment.getId());
 	}
 
 }
--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -12,7 +12,7 @@ public class Department implements Serializable {
 	public Department() {
 	}
 
-	public Department(int id, String name) {
+	public Department(Integer id, String name) {
 		this.id = id;
 		this.name = name;
 	}
