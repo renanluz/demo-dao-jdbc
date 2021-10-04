521d0b2e35b93dce6e9c50f7d763ce6a4ec7aaa5
Renan Luz <ren4n@live.com>
2021-10-04 07:55:01 -0300

    update implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -18,9 +18,15 @@ public class Program2 {
 //		
 		
 		
-		System.out.println("\n===== TEST 1: department findById =====");
-		Department department = departmentDao.findById(3);
-		System.out.println(department);
+//		System.out.println("\n===== TEST 2: department findById =====");
+//		Department department = departmentDao.findById(3);
+//		System.out.println(department);
+		
+		System.out.println("\n===== TEST 3: department update =====");	
+		Department department = departmentDao.findById(6);
+		department.setName("Development");
+		departmentDao.update(department);
+		System.out.println("Update completed");
 		
 		
 	}
